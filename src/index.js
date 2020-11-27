912a92cc312d913e7cd74fc7206ff7b444244bfe
Bruk Zewdie <bbz2103@columbia.edu>
2020-11-27 14:35:02 -0500

    add containsObsceneWord and removeObscenity functions

--- src/index.js
+++ src/index.js
@@ -13,7 +13,58 @@ function getLabels(word) {
   return labels;
 }
 
+function containsObsceneWord(phrase) {
+  const words = phrase.trim().split(/\s+/);
+
+  for (let i = 0; i < words.length; i += 1) {
+    if (!isPure(words[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function removeObscenityFromString(phrase) {
+  const words = phrase.trim().split(/\s+/);
+
+  const filtered = [];
+  for (let i = 0; i < words.length; i += 1) {
+    if (isPure(words[i])) {
+      filtered.push(words[i]);
+    } else {
+      filtered.push('*'.repeat(words[i].length));
+    }
+  }
+
+  return filtered.join(' ');
+}
+
+function removeObscenityFromArray(words) {
+  const filtered = [];
+  for (let i = 0; i < words.length; i += 1) {
+    if (isPure(words[i])) {
+      filtered.push(words[i]);
+    }
+  }
+  return filtered;
+}
+
+function removeObscenity(words) {
+  if (typeof (words) === 'string') {
+    return removeObscenityFromString(words);
+  }
+
+  if (Array.isArray(words)) {
+    return removeObscenityFromArray(words);
+  }
+
+  throw new TypeError();
+}
+
 module.exports = {
+  containsObsceneWord,
   getLabels,
   isPure,
+  removeObscenity,
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { getLabels, isPure } = require('../src/index');
+const {
+  containsObsceneWord, getLabels, isPure, removeObscenity,
+} = require('../src/index');
 
 const BAD_WORDS = [
   'ብድ',
@@ -9,6 +11,15 @@ const BAD_WORDS = [
 
 const GOOD_WORDS = [
   'ብድሬን',
+];
+
+const GOOD_PHRASES = [
+  'ልዑኩ ትናንት አዲስ አበባ የገባው በኢትዮጵያ መንግሥትና በትግራይ ክልል ኃይሎች መካከል እየተካሄደ ያለው ግጭት ሦስት ሳምንት ካስቆጠረ እና የመጨረሻ የተባለው ዘመቻ እንዲካሄድ ጠቅላይ ሚንሰትሩ ካዘዙ በኋላ ነው።',
+  'በዚህ መሰል የምርመራ ሥራ ላይ ተሳትፎ     ሲደረግ    በተቻለ መጠን ግላዊ  ስሜትን ወደ ጎን በማድረግ ሙያዊ ሥራ ላይ ማተኮር ይጠበቃል',
+];
+
+const BAD_PHRASES = [
+  'አንዳንድ ሰዎች በማህበራዊ ገጽ ላይ እየገቡ ብዳታም እያሉ ይሳደባሉ',
 ];
 
 describe('wordCheck', () => {
@@ -50,6 +61,14 @@ describe('wordCheck', () => {
 });
 
 describe('wordCheck', () => {
+  describe('isPure', () => {
+    it('empty string should return false', () => {
+      assert.equal(isPure(''), true);
+    });
+  });
+});
+
+describe('wordCheck', () => {
   describe('getLabels', () => {
     it('should return an empty list for "non-obscene" Amharic words', () => {
       for (let i = 0; i < GOOD_WORDS.length; i += 1) {
@@ -64,6 +83,68 @@ describe('wordCheck', () => {
   describe('getLabels', () => {
     it('should return a list of the matching labels', () => {
       assert.deepEqual(getLabels(BAD_WORDS[0]), ['PN']);
+    });
+  });
+});
+
+describe('phraseCheck', () => {
+  describe('containsObsceneWord', () => {
+    it('should return true for sentences that contain obscenity', () => {
+      for (let i = 0; i < BAD_PHRASES.length; i += 1) {
+        const phrase = BAD_PHRASES[i];
+        assert.equal(containsObsceneWord(phrase), true);
+      }
+    });
+  });
+});
+
+describe('phraseCheck', () => {
+  describe('containsObsceneWord', () => {
+    it('should return false for sentences with no obscene words in them', () => {
+      for (let i = 0; i < GOOD_PHRASES.length; i += 1) {
+        const phrase = GOOD_PHRASES[i];
+        assert.equal(containsObsceneWord(phrase), false);
+      }
+    });
+  });
+});
+
+describe('filterWords', () => {
+  describe('removeObscenity', () => {
+    it('should not remove anything except whitespaces for phrases with no obscenity', () => {
+      for (let i = 0; i < GOOD_PHRASES.length; i += 1) {
+        const phrase = GOOD_PHRASES[i];
+        const trimmedWords = phrase.trim().split(/\s+/);
+        assert.equal(removeObscenity(phrase), trimmedWords.join(' '));
+      }
+    });
+  });
+});
+
+describe('filterWords', () => {
+  describe('removeObscenity', () => {
+    it('should remove all obscene words from string', () => {
+      const rawPhrase = 'አንዳንድ ሰዎች በማህበራዊ ገጽ ላይ እየገቡ ብዳታም እያሉ ይሳደባሉ';
+      const cleanedPhrase = 'አንዳንድ ሰዎች በማህበራዊ ገጽ ላይ እየገቡ **** እያሉ ይሳደባሉ';
+      assert.equal(removeObscenity(rawPhrase), cleanedPhrase);
+    });
+  });
+});
+
+describe('filterWords', () => {
+  describe('removeObscenity', () => {
+    it('should return non-obscene words only from array', () => {
+      const rawPhrase = 'አንዳንድ ሰዎች በማህበራዊ ገጽ ላይ እየገቡ ብዳታም እያሉ ይሳደባሉ';
+      const cleanedPhrase = 'አንዳንድ ሰዎች በማህበራዊ ገጽ ላይ እየገቡ እያሉ ይሳደባሉ';
+      assert.deepEqual(removeObscenity(rawPhrase.split(/\s+/)), cleanedPhrase.split(/\s+/));
+    });
+  });
+});
+
+describe('filterWords', () => {
+  describe('removeObscenity', () => {
+    it('throws TypeError for arguments of neither string nor array', () => {
+      assert.throws(() => removeObscenity({ key: 'value' }), TypeError);
     });
   });
 });
